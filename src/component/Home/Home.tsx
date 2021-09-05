3b956e61f5b05989dfb0d7eba063632451afa1bd
Evvvai <66115913+Evvvai@users.noreply.github.com>
2021-09-06 02:09:15 +0400

    schedule structure

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -4,19 +4,12 @@ import React, { FC } from 'react'
 // Interface
 interface Props {}
 
-const schedule = [1, 2, 3, 4, 5, 6]
-
 // Component
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const Home: FC<Props> = (props) => {
   return (
     <section className="home">
       <div className="home__date">DATE</div>
-      <div className="home__content">
-        {schedule.map((val, key) => {
-          return <div className="home-item"></div>
-        })}
-      </div>
     </section>
   )
 }
