5556811f2cbf369702babd6f1d56c68eadd0e0be
vaetimidis <seth.wizard@bk.ru>
2021-09-05 22:17:42 +0400

    draft

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,23 +1,25 @@
-import "./App.scss";
-import React, { FC } from "react";
-import { Route, Switch } from "react-router-dom";
+import './App.scss'
+import React, { FC } from 'react'
+import { Route, Switch } from 'react-router-dom'
 
 // Import components
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-import Home from "./Home/Home";
+import Home from './Home/Home'
+import Header from './Header/Header'
 
 // Component
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const App: FC<any> = () => {
   return (
     <>
+      <Header />
       <main className="main">
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -1,17 +1,24 @@
-import "./Home.scss";
-import React, { FC } from "react";
+import './Home.scss'
+import React, { FC } from 'react'
 
 // Interface
 interface Props {}
+
+const schedule = [1, 2, 3, 4, 5, 6]
 
 // Component
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const Home: FC<Props> = (props) => {
   return (
     <section className="home">
-      <div className="home__text">Iatu mind</div>
+      <div className="home__date">DATE</div>
+      <div className="home__content">
+        {schedule.map((val, key) => {
+          return <div className="home-item"></div>
+        })}
+      </div>
     </section>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
