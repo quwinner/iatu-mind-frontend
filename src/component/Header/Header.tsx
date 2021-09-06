cd925b4e8fc3aad3f282177e3467c2bd42fe7903
Evvvai <66115913+Evvvai@users.noreply.github.com>
2021-09-06 14:54:35 +0400

    Shedule ui

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,6 +1,5 @@
 import './Header.scss'
 import React, { FC } from 'react'
-import menu from '../../utils/img/menu_black_24dp.svg'
 
 // Interface
 interface Props {}
@@ -10,9 +9,7 @@ interface Props {}
 const Header: FC<Props> = (props) => {
   return (
     <header className="header">
-      <div className="header__menu">
-        <img src={menu} alt="menu" />
-      </div>
+      <div className="header__menu">{/* <img src={menu} alt="menu" /> */}</div>
       <div className="header__logo">IATU-MIND</div>
     </header>
   )
--- src/component/Schedule/Schedule.tsx
+++ src/component/Schedule/Schedule.tsx
@@ -4,6 +4,9 @@ import React, { FC } from 'react'
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Import Component
 import ScheduleItem from './ScheduleItem/ScheduleItem'
+import ScheduleTime from './ScheduleTime/ScheduleTime'
+
+import { ReactComponent as Arrow } from '../../utils/img/arrow_left.svg'
 
 // Interface
 interface Props {}
@@ -15,10 +18,22 @@ const schedule = [1, 2, 3, 4, 5, 6]
 const Schedule: FC<Props> = (props) => {
   return (
     <section className="schedule">
-      <div className="schedule__date">DATE</div>
-      <div className="schedule__content">
+      <div className="schedule-date">
+        <div className="schedule-arrow-pagination">
+          <Arrow className="schedule-arrow-pagination__icon back" />
+          <div className="schedule-arrow-pagination__text">Предыдущая неделя</div>
+        </div>
+        <div className="schedule-date__header">6 Сентября - 10 Сентября</div>
+        <div className="schedule-arrow-pagination">
+          <div className="schedule-arrow-pagination__text">Следующая неделя</div>
+          <Arrow className="schedule-arrow-pagination__icon next" />
+        </div>
+      </div>
+      <div className="schedule-today">Сегодня 5 Сентября</div>
+      <ScheduleTime />
+      <div className="schedule-content">
         {schedule.map((val, key) => {
-          return <ScheduleItem item={val} />
+          return <ScheduleItem key={key} date={val} />
         })}
       </div>
     </section>
