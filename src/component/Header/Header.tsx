92e7da382937d2e36e2015cd9ec19983cdae89d5
vaetimidis <seth.wizard@bk.ru>
2021-09-06 20:32:24 +0400

    left side var(ne dodelal, ya loh)

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,7 +1,10 @@
 import './Header.scss'
 import React, { FC } from 'react'
-import { ReactComponent as Menu } from '../../utils/img/menu_white.svg'
+import { ReactComponent as MenuRight } from '../../utils/img/menu_white.svg'
 import { ReactComponent as DownArrow } from '../../utils/img/expand_more_white.svg'
+import { ReactComponent as ArrowForward } from '../../utils/img/arrow_forward.svg'
+
+import Menu from './Menu/Menu'
 
 // Interface
 interface Props {}
@@ -11,15 +14,15 @@ interface Props {}
 const Header: FC<Props> = (props) => {
   return (
     <header className="header">
+      <ArrowForward className="header__btn" />
       <div className="header__tab">Расписание</div>
       <div className="profile-header">
         <div className="profile-header__group">АИСТбд-31</div>
-        <div className="profile-header__downarrow">
-          <DownArrow />
-        </div>
+        <DownArrow className="profile-header__downarrow" />
         <div className="profile-header__avatar">a</div>
         <div className="profile-header__menu">
-          <Menu />
+          <MenuRight />
+          {/* по другому не работает, ошибка хз где */}
         </div>
       </div>
     </header>
