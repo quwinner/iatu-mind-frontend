ad4764571fb1fda40ac4b8841b3e532c4d2ef28f
vaetimidis <seth.wizard@bk.ru>
2021-09-13 22:29:03 +0400

    search bar v govne

--- src/component/Header/SearchBar/SearchBar.tsx
+++ src/component/Header/SearchBar/SearchBar.tsx
@@ -1,10 +1,12 @@
 import './SearchBar.scss'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import cn from 'classnames'
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Import Component
 
 // Utils
+import { ReactComponent as Search } from '../../../utils/img/search_white.svg'
 
 // Interface
 interface Props {}
@@ -12,9 +14,31 @@ interface Props {}
 // Component
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const SearchBar: FC<Props> = (props) => {
+  const [state, setState] = useState<boolean>(false)
+
   return (
-    <div className="search-bar">
-      <div></div>
+    <div className="search">
+      <div className="search-bar">
+        <input
+          type="text"
+          className={cn('search-bar__input', { active: state })}
+          placeholder="Найти..."
+          onClick={() => setState(!state)}
+        />
+        <div className="search-bar__loupe">
+          <Search />
+        </div>
+        <section onClick={() => setState(false)} className={cn('search-bar__section ', { active: state })}>
+          {/* Я конечно не уверен, но мне кажется он выводит скрывающийся блок 
+              так криво потому что там кнопку не видно из за бекэнда */}
+          <div className="search-info">
+            <div className="search-info___chapter">Расписание</div>
+            <div>Профиль</div>
+            <div>Убить себя</div>
+            <div>Убить преподавателя</div>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
