83ace142d74aa84456d52dd5f5c31c3f22a1bccb
Evvvai <66115913+Evvvai@users.noreply.github.com>
2021-09-14 16:55:07 +0400

    Loading

--- src/component/Schedule/ScheduleTime/ScheduleTime.tsx
+++ src/component/Schedule/ScheduleTime/ScheduleTime.tsx
@@ -10,6 +10,7 @@ import SchedulePair from '../SchedulePair/SchedulePair'
 // Custom hook
 import { useSchedule } from '../../../hook/useSchedule'
 import { useApplication } from '../../../hook/useApplication'
+import { Schedule } from '../../../types'
 
 // Interface
 interface Props {}
@@ -19,13 +20,25 @@ interface Props {}
 const ScheduleTime: FC<Props> = (props) => {
   // States
   const [seconds, setSeconds] = useState<number>(0)
-  const [extend, setExtend] = useState<boolean>(false)
+  const [extendNext, setExtendNext] = useState<boolean>(false)
+  const [extendNow, setExtendNow] = useState<boolean>(false)
 
-  const { schedule, getNextPair, nextPair } = useSchedule()
+  const { schedule, getNextPair, nextPair, nowPair } = useSchedule()
   const { groupNow } = useApplication()
+
+  const validateNowPair = (pair: Schedule): boolean => {
+    const timeNow = dayjs(new Date()).format('HH:mm:ss')
+
+    if (pair === undefined) return false
+    return pair.timeStop > timeNow && timeNow < pair.timeStop
+  }
 
   useEffect(() => {
     getNextPair(groupNow)
+    setSeconds(0)
+
+    // console.log(nextPair[0]?.timeStart, dayjs(new Date()).format('HH:mm'), nextPair[0]?.timeStop)
+
     // const d = dayjs(new Date()).format('YYYY.MM.DD')
     // const t = dayjs(new Date()).format('HH:mm')
     // const next = schedule.find((x) => {
@@ -33,7 +46,8 @@ const ScheduleTime: FC<Props> = (props) => {
     //   return dayjs(x.date).format('YYYY.MM.DD') >= d && x.timeStart < t
     // })
     // console.log(dayjs().diff(next?.timeStart, 'second'))
-  }, [schedule])
+    // console.log(nextPair, nowPair)
+  }, [groupNow])
 
   useEffect(() => {
     if (nextPair[0]?.timeWait !== undefined && nextPair[0].timeWait - seconds <= 0) {
@@ -45,18 +59,32 @@ const ScheduleTime: FC<Props> = (props) => {
   }, [seconds])
 
   return (
-    <div onClick={(e) => setExtend(!extend)} className="schedule-time">
-      <div className="schedule-time__header">
-        <p>До следующей пары </p>
-        <p>
+    <div className="schedule-time">
+      <hr className="schedule-time__hr" />
+      <div onClick={(e) => setExtendNext(!extendNext)} className="schedule-time__header">
+        <span className="schedule-time-span base">До следующей пары </span>
+        <span className="schedule-time-span darker time">
           {nextPair[0]?.timeWait
             ? new Date((nextPair[0].timeWait - seconds) * 1000).toISOString().substr(11, 8)
             : 'неизвестно'}
-        </p>
+        </span>
       </div>
-      <div className={cn('schedule-next', { extend: extend })}>
+      <div className={cn('schedule-next', { extend: extendNext })}>
         {nextPair[0]?.timeWait !== undefined && <SchedulePair extend={true} schedule={nextPair} />}
       </div>
+      {validateNowPair(nowPair[0]) && (
+        <>
+          <div onClick={(e) => setExtendNow(!extendNow)} className="schedule-time__header">
+            <span className="schedule-time-span base">До окончания пары </span>
+            <span className="schedule-time-span darker time">
+              {nowPair[0]?.timeWait ? new Date((nowPair[0].timeWait - seconds) * 1000).toISOString().substr(11, 8) : ''}
+            </span>
+          </div>
+          <div className={cn('schedule-next', { extend: extendNow })}>
+            {nowPair[0]?.timeWait !== undefined && <SchedulePair extend={true} schedule={nowPair} />}
+          </div>
+        </>
+      )}
     </div>
   )
 }
