98e537efa42acb571fcd2eed44ce406fddd97361
Evvvai <66115913+Evvvai@users.noreply.github.com>
2021-09-15 17:32:24 +0400

    Footer

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -1,9 +1,10 @@
 import './Home.scss'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Import Component
 import Loading from '../Loading/Loading'
+import ScheduleTicket from '../Schedule/ScheduleTicket/ScheduleTicket'
 
 // Interface
 interface Props {}
@@ -11,9 +12,18 @@ interface Props {}
 // Component
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const Home: FC<Props> = (props) => {
+  const [ticketShow, setTicketShow] = useState<boolean>(false)
+
   return (
-    <section className="home">
+    <section
+      onClick={(e: any) => {
+        if (ticketShow) return
+        setTicketShow(true)
+      }}
+      className="home"
+    >
       <Loading />
+      {ticketShow && <ScheduleTicket setShow={setTicketShow} />}
     </section>
   )
 }
